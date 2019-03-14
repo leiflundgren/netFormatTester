ad9cab1180c0201e62bffca7e44d893e43e364bf
Leif Lundgren <leif.lundgren@mitel.com>
2019-03-14 11:17:08 +0100

    Code, with local modifications

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Formatter
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+            this.OKButton = new System.Windows.Forms.Button();
+            this.FormatStrings = new System.Windows.Forms.ComboBox();
+            this.FormatLabel = new System.Windows.Forms.Label();
+            this.DateBox = new System.Windows.Forms.RadioButton();
+            this.NumberBox = new System.Windows.Forms.RadioButton();
+            this.StatusBar = new System.Windows.Forms.StatusStrip();
+            this.CultureNames = new System.Windows.Forms.ComboBox();
+            this.CulturesLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.ValueLabel = new System.Windows.Forms.Label();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.chkContinuous = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OKButton
+            // 
+            this.OKButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.OKButton.Location = new System.Drawing.Point(373, 227);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(41, 23);
+            this.OKButton.TabIndex = 17;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            this.OKButton.Click += new System.EventHandler(this.OKButton_Click);
+            // 
+            // FormatStrings
+            // 
+            this.FormatStrings.FormattingEnabled = true;
+            this.FormatStrings.Location = new System.Drawing.Point(17, 25);
+            this.FormatStrings.Name = "FormatStrings";
+            this.FormatStrings.Size = new System.Drawing.Size(192, 21);
+            this.FormatStrings.TabIndex = 14;
+            this.FormatStrings.SelectedIndexChanged += new System.EventHandler(this.FormatStrings_SelectedIndexChanged);
+            // 
+            // FormatLabel
+            // 
+            this.FormatLabel.AutoSize = true;
+            this.FormatLabel.Location = new System.Drawing.Point(14, 9);
+            this.FormatLabel.Name = "FormatLabel";
+            this.FormatLabel.Size = new System.Drawing.Size(90, 13);
+            this.FormatLabel.TabIndex = 13;
+            this.FormatLabel.Text = "FormatComboBox";
+            // 
+            // DateBox
+            // 
+            this.DateBox.AutoSize = true;
+            this.DateBox.Location = new System.Drawing.Point(281, 42);
+            this.DateBox.Name = "DateBox";
+            this.DateBox.Size = new System.Drawing.Size(90, 17);
+            this.DateBox.TabIndex = 12;
+            this.DateBox.TabStop = true;
+            this.DateBox.Text = "RadioButton2";
+            this.DateBox.UseVisualStyleBackColor = true;
+            this.DateBox.CheckedChanged += new System.EventHandler(this.DateBox_CheckedChanged);
+            // 
+            // NumberBox
+            // 
+            this.NumberBox.AutoSize = true;
+            this.NumberBox.Location = new System.Drawing.Point(281, 18);
+            this.NumberBox.Name = "NumberBox";
+            this.NumberBox.Size = new System.Drawing.Size(90, 17);
+            this.NumberBox.TabIndex = 11;
+            this.NumberBox.TabStop = true;
+            this.NumberBox.Text = "RadioButton1";
+            this.NumberBox.UseVisualStyleBackColor = true;
+            this.NumberBox.CheckedChanged += new System.EventHandler(this.NumberBox_CheckedChanged);
+            // 
+            // StatusBar
+            // 
+            this.StatusBar.Location = new System.Drawing.Point(0, 253);
+            this.StatusBar.Name = "StatusBar";
+            this.StatusBar.Size = new System.Drawing.Size(426, 22);
+            this.StatusBar.TabIndex = 18;
+            this.StatusBar.Text = "StatusStrip1";
+            // 
+            // CultureNames
+            // 
+            this.CultureNames.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CultureNames.FormattingEnabled = true;
+            this.CultureNames.Location = new System.Drawing.Point(17, 75);
+            this.CultureNames.Name = "CultureNames";
+            this.CultureNames.Size = new System.Drawing.Size(192, 21);
+            this.CultureNames.TabIndex = 20;
+            this.CultureNames.SelectedIndexChanged += new System.EventHandler(this.CultureNames_SelectedIndexChanged);
+            // 
+            // CulturesLabel
+            // 
+            this.CulturesLabel.AutoSize = true;
+            this.CulturesLabel.Location = new System.Drawing.Point(14, 59);
+            this.CulturesLabel.Name = "CulturesLabel";
+            this.CulturesLabel.Size = new System.Drawing.Size(91, 13);
+            this.CulturesLabel.TabIndex = 19;
+            this.CulturesLabel.Text = "CultureComboBox";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.Result, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.ValueLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtValue, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.ResultLabel, 0, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(17, 102);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(350, 148);
+            this.tableLayoutPanel1.TabIndex = 21;
+            // 
+            // Result
+            // 
+            this.Result.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Result.Location = new System.Drawing.Point(3, 97);
+            this.Result.Multiline = true;
+            this.Result.Name = "Result";
+            this.Result.ReadOnly = true;
+            this.Result.Size = new System.Drawing.Size(344, 48);
+            this.Result.TabIndex = 18;
+            // 
+            // ValueLabel
+            // 
+            this.ValueLabel.AutoSize = true;
+            this.ValueLabel.Location = new System.Drawing.Point(3, 0);
+            this.ValueLabel.Name = "ValueLabel";
+            this.ValueLabel.Size = new System.Drawing.Size(73, 13);
+            this.ValueLabel.TabIndex = 10;
+            this.ValueLabel.Text = "ValueTextBox";
+            // 
+            // txtValue
+            // 
+            this.txtValue.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtValue.Location = new System.Drawing.Point(3, 23);
+            this.txtValue.Multiline = true;
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(344, 48);
+            this.txtValue.TabIndex = 11;
+            this.txtValue.TextChanged += new System.EventHandler(this.txtValue_TextChanged);
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(3, 74);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(76, 13);
+            this.ResultLabel.TabIndex = 17;
+            this.ResultLabel.Text = "ResultTextBox";
+            // 
+            // chkContinuous
+            // 
+            this.chkContinuous.AutoSize = true;
+            this.chkContinuous.Checked = true;
+            this.chkContinuous.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkContinuous.Location = new System.Drawing.Point(281, 66);
+            this.chkContinuous.Name = "chkContinuous";
+            this.chkContinuous.Size = new System.Drawing.Size(79, 17);
+            this.chkContinuous.TabIndex = 22;
+            this.chkContinuous.Text = "Continuous";
+            this.chkContinuous.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 275);
+            this.Controls.Add(this.chkContinuous);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.CultureNames);
+            this.Controls.Add(this.CulturesLabel);
+            this.Controls.Add(this.StatusBar);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.FormatStrings);
+            this.Controls.Add(this.FormatLabel);
+            this.Controls.Add(this.DateBox);
+            this.Controls.Add(this.NumberBox);
+            this.Name = "Form1";
+            this.Text = "Format test util";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+      }
+
+      #endregion
+
+      internal System.Windows.Forms.Button OKButton;
+      internal System.Windows.Forms.ComboBox FormatStrings;
+      internal System.Windows.Forms.Label FormatLabel;
+      internal System.Windows.Forms.RadioButton DateBox;
+      internal System.Windows.Forms.RadioButton NumberBox;
+      internal System.Windows.Forms.StatusStrip StatusBar;
+      internal System.Windows.Forms.ComboBox CultureNames;
+      internal System.Windows.Forms.Label CulturesLabel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        internal System.Windows.Forms.TextBox Result;
+        internal System.Windows.Forms.Label ValueLabel;
+        internal System.Windows.Forms.TextBox txtValue;
+        internal System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.CheckBox chkContinuous;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Numerics;
+using System.Reflection;
+using System.Resources;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Formatter
+{
+   public partial class Form1 : Form
+   {
+      private ResourceManager rm;
+
+      private ToolStripStatusLabel label;
+
+      private string decimalSeparator;
+      private string amDesignator, pmDesignator, aDesignator, pDesignator;
+      private string pattern;
+
+      // Flags to indicate presence of error information in status bar
+      bool valueInfo;
+      bool formatInfo;
+
+      private string[] numberFormats = {"C", "D", "E", "e", "F", "G", "N", "P", "R", "X", "x"};
+      private const int DEFAULTSELECTION = 5;
+      private string[] dateFormats = { "g", "d", "D", "f", "F", "g", "G", "M", "O", "R", "s", 
+                                       "t", "T", "u", "U", "Y" };
+
+      public Form1()
+      {
+         InitializeComponent();
+         rm = new ResourceManager("Formatter.Resources", this.GetType().Assembly);
+      }
+
+      private void Form1_Load(object sender, EventArgs e)
+      {
+         // Disable Value text box.
+         OKButton.Enabled = false;
+
+         // Add label to status bar.
+         label = new ToolStripStatusLabel();
+         ToolStripItem[] items = { label };
+         this.StatusBar.Items.AddRange( items);
+
+         // Get localized strings for user interface.
+         this.Text = rm.GetString("WindowCaption");
+         this.ValueLabel.Text = rm.GetString("ValueLabel");
+         this.FormatLabel.Text = rm.GetString("FormatLabel");
+         this.ResultLabel.Text = rm.GetString("ResultLabel");
+         this.CulturesLabel.Text = rm.GetString("CultureLabel");
+         this.NumberBox.Text = rm.GetString("NumberBoxText");
+         this.DateBox.Text = rm.GetString("DateBoxText");
+         this.OKButton.Text = rm.GetString("OKButtonText");
+
+         // Populate CultureNames list box with culture names
+      CultureInfo[] cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+      // Define a string array so that we can sort and modify the names.
+      List<String> names = new List<String>();
+      int currentIndex = 0;                    // Index of the current culture.
+
+      foreach (var culture in cultures)
+         names.Add(culture.Name);
+
+      names.Sort();
+      // Change the name of the invariant culture so it is human readable.
+      names[0] = rm.GetString("InvariantCultureName");
+      // Add the culture names to the list box.
+      this.CultureNames.Items.AddRange(names.ToArray());
+
+      // Make the current culture the selected culture.
+      for (int ctr = 0; ctr < names.Count; ctr++) {
+         if (names[ctr] == CultureInfo.CurrentCulture.Name) {
+            currentIndex = ctr;
+            break;
+         }
+      }
+      this.CultureNames.SelectedIndex = currentIndex;
+
+         // Get decimal separator.
+         decimalSeparator = NumberFormatInfo.CurrentInfo.NumberDecimalSeparator;
+
+         // Get am, pm designators.
+         amDesignator = DateTimeFormatInfo.CurrentInfo.AMDesignator;
+         if (amDesignator.Length >= 1)
+            aDesignator = amDesignator.Substring(0, 1);
+         else
+            aDesignator = String.Empty;
+
+         pmDesignator = DateTimeFormatInfo.CurrentInfo.PMDesignator;
+         if (pmDesignator.Length >= 1)
+            pDesignator = pmDesignator.Substring(0, 1);
+         else
+            pDesignator = String.Empty;
+
+         // For regex pattern for date and time components.
+         pattern = @"^\s*\S+\s+\S+\s+\S+(\s+\S+)?(?<!" + amDesignator + "|" + 
+                   aDesignator + "|" + pmDesignator + "|" + pDesignator + @")\s*$";
+
+         // Select NumberBox for numeric string and populate combo box.
+         this.NumberBox.Checked = true;
+      }
+
+      private void NumberBox_CheckedChanged(object sender, EventArgs e)
+      {
+         if (this.NumberBox.Checked) {
+            this.Result.Text = String.Empty;
+
+            this.FormatStrings.Items.Clear();
+            this.FormatStrings.Items.AddRange(numberFormats);
+            this.FormatStrings.SelectedIndex = DEFAULTSELECTION;
+         }
+      }
+
+      private void OKButton_Click(object sender, EventArgs e)
+        {
+            Execute();
+        }
+
+        private void Execute()
+        {
+            label.Text = "";
+            this.Result.Text = String.Empty;
+
+            // Get name of the current culture.
+            CultureInfo culture = null;
+            string cultureName = (string)this.CultureNames.Items[this.CultureNames.SelectedIndex];
+            // If the selected culture is the invariant culture, change its name.
+            if ( cultureName == rm.GetString("InvariantCultureName") )
+                cultureName = String.Empty;
+            culture = CultureInfo.CreateSpecificCulture(cultureName);
+
+            foreach ( string txt in txtValue.Text.Replace("\r\n", "\n").Split('\n') )
+            {
+                try
+                {
+                    this.Result.Text += Format(txt, culture) + "\r\n";
+                }
+                catch ( FormatException ex )
+                {
+                    label.Text = ex.Message;
+                    formatInfo = true;
+                }
+                catch ( ArgumentException ex )
+                {
+                    label.Text = ex.Message;
+                    valueInfo = true;
+                }
+            }
+        }
+
+        private string Format(string txt, CultureInfo culture)
+        {
+            // Parse string as date
+            if ( this.DateBox.Checked )
+            {
+                DateTime dat = DateTime.MinValue;
+                DateTimeOffset dto = DateTimeOffset.MinValue;
+                long ticks;
+                bool hasOffset = false;
+
+                // Is the date a number expressed in ticks?
+                if ( Int64.TryParse(txt, out ticks) )
+                {
+                    dat = new DateTime(ticks);
+                }
+                else
+                {
+                    // Does the date have three components (date, time offset), or fewer than 3?
+                    if ( Regex.IsMatch(txt, pattern, RegexOptions.IgnoreCase) )
+                    {
+                        if ( !DateTimeOffset.TryParse(txt, out dto) )
+                            throw new ArgumentException(rm.GetString("MSG_InvalidDTO"));
+                            hasOffset = true;
+                        
+                    }
+                    else
+                    {
+                        // The string is to be interpeted as a DateTime, not a DateTimeOffset.
+                        if ( DateTime.TryParse(txt, out dat) )
+                        {
+                            hasOffset = false;
+                        }
+                        else
+                        {
+                            throw new ArgumentException(rm.GetString("MSG_InvalidDate"));
+                        }
+                    }
+                }            // Format date value.
+                if ( hasOffset )
+                    return dto.ToString(this.FormatStrings.Text, culture);
+                else
+                    return dat.ToString(this.FormatStrings.Text, culture);
+            }
+            else
+            {
+                // Handle formatting of a number.
+                long intToFormat;
+                BigInteger bigintToFormat = BigInteger.Zero;
+                double floatToFormat;
+
+                // Format a floating point value.
+                if ( txt.Contains(decimalSeparator) )
+                {
+                    try
+                    {
+                        if ( !Double.TryParse(txt, out floatToFormat) )
+                            throw new ArgumentException(rm.GetString("MSG_InvalidFloat"));
+                        else
+                            return floatToFormat.ToString(this.FormatStrings.Text, culture);
+                    }
+                    catch ( FormatException )
+                    {
+                        throw new FormatException(rm.GetString("MSG_InvalidFormat"));
+                    }
+                }
+                else
+                {
+                    // Handle formatting an integer.
+                    //
+                    // Determine whether value is out of range of an Int64
+                    if ( !BigInteger.TryParse(txt, out bigintToFormat) )
+                    {
+                        throw new ArgumentException(rm.GetString("MSG_InvalidInteger"));
+                    }
+                    else
+                    {
+                        // Format an Int64
+                        if ( bigintToFormat >= Int64.MinValue && bigintToFormat <= Int64.MaxValue )
+                        {
+                            intToFormat = (long)bigintToFormat;
+                            try
+                            {
+                                return intToFormat.ToString(this.FormatStrings.Text, culture);
+                            }
+                            catch ( FormatException )
+                            {
+                                throw new FormatException(rm.GetString("MSG_InvalidFormat"));
+                            }
+                        }
+                        else
+                        {
+                            // Format a BigInteger
+                            try
+                            {
+                                return bigintToFormat.ToString(this.FormatStrings.Text, culture);
+                            }
+                            catch ( FormatException )
+                            {
+                                throw new FormatException(rm.GetString("MSG_InvalidFormat"));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void DateBox_CheckedChanged(object sender, EventArgs e)
+      {
+         if (this.DateBox.Checked) {
+            this.Result.Text = String.Empty;
+
+            this.FormatStrings.Items.Clear();
+            this.FormatStrings.Items.AddRange(dateFormats);
+            this.FormatStrings.SelectedIndex = DEFAULTSELECTION;
+         }
+      }
+
+      private void txtValue_TextChanged(object sender, EventArgs e)
+      {
+         this.Result.Text = String.Empty;
+
+         if (valueInfo) { 
+            label.Text = String.Empty;
+            valueInfo = false;
+         }
+            if ( String.IsNullOrEmpty(txtValue.Text) )
+            {
+                OKButton.Enabled = false;
+            }
+            else
+            {
+                OKButton.Enabled = true;
+                if ( chkContinuous.Checked )
+                    Execute();
+            }
+      }
+
+      private void FormatStrings_SelectedIndexChanged(object sender, EventArgs e)
+      {
+         this.Result.Text = String.Empty;
+         if (formatInfo) {
+            label.Text = String.Empty;
+            formatInfo = false;
+         }
+      }
+
+      private void CultureNames_SelectedIndexChanged(object sender, EventArgs e)
+      {
+         this.Result.Text = String.Empty;
+      }
+   }
+}
